ab667c1e6afd5b6279566a90784f56d0d99c0617
gooddaysyk <seoyk99@gmail.com>
2025-04-02 06:10:48 +0900

    Firebase 설정 수정: API 키 및 설정 업데이트

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "dailythought-c8a5c",
   storageBucket: "dailythought-c8a5c.appspot.com",
   messagingSenderId: "1098498831961",
-  appId: "1:1098498831961:web:e2c0c0a5a2c0a5a2c0a5a2"
+  appId: "1:1098498831961:web:d4e0c0a5a2c0a5a2c0a5a2"
 };
 
 // Firebase 초기화
